d376bda9fe4936a6a21a59f7a4aa22168475cdb9
xiften <108333567+not-ani@users.noreply.github.com>
2024-03-26 15:20:14 -0600

    changed Discord to Google for auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -72,8 +72,8 @@ export const authOptions: NextAuthOptions = {
   adapter: DrizzleAdapter(db) as Adapter,
   providers: [
     DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
